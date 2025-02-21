786692f458c9541de738140f2e7b938162190fc7
barmen4ik <onlinemonsterarmy@gmail.com>
2025-02-21 20:58:23 +0200

    commi-3

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-print("hello world")
-print("Hello Git")
+my_lucky_number = 7
+while True:
+    try:
+        guess = int(input("Guess my lucky number! I think it is: "))
+        if guess == my_lucky_number:
+            print("Congrats! You guessed it.")
+            break
+    except ValueError:
+        print("Please enter a valid number!")

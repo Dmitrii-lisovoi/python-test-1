3667128c8ec0e436bf95a419d44f46597f47aad6
barmen4ik <onlinemonsterarmy@gmail.com>
2025-02-22 23:55:06 +0200

    commit-4

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-my_lucky_number = 7
-while True:
-    try:
-        guess = int(input("Guess my lucky number! I think it is: "))
-        if guess == my_lucky_number:
-            print("Congrats! You guessed it.")
-            break
-    except ValueError:
-        print("Please enter a valid number!")
+#Ціна на товар
+price_per_croissant = 1.04
+price_per_glass = 0.34
+price_per_coffe_pack = 4.42
+#Кількістькожного продукту
+num_croissant = int(input("Введіть кількість круасанів:"))
+num_glasses = int(input("Введіть кількість стаканчиків:"))
+num_coffe_pack = int(input("Введіть кількість упаковок кави:"))
+#Обчислення загальної вартості
+total_cost = num_croissant * price_per_croissant + num_glasses * price_per_glass + num_coffe_pack * price_per_coffe_pack
+#кількість повних доларів і центів
+total_dollars = int(total_cost)
+total_cents = int(total_cost * 100)
+#Вивід результату
+print(f"Загальна вартість у повних доларах: {total_dollars}")
+print(f"Загальна Вартість у центах: {total_cents}")
